d4ba751804cf03affa37b26f261ab29cc7af7ba1
IbrahimSawani <92049371+IbrahimSawani@users.noreply.github.com>
2021-10-07 14:52:30 +0500

    Add files via upload

---
+++ Assignment # 1 - 3 Equations of Motion.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# First Equation of Motion
+u=5
+a=2
+t=10
+v=u+a*t
+
+print("First Equation of Motion is "+str(v))
+
+
+# In[4]:
+
+
+# Second Equation of Motion
+u=5
+a=2
+t=10
+s=u*t+1/2*(a*(t*t))
+
+print("Second Equation of Motion is "+str(s))
+
+
+# In[5]:
+
+
+# Third Equation of Motion
+u=5
+a=2
+t=10
+s=((v*v)-(u*u))/(2*a)
+
+print("Third Equation of Motion is "+str(s))
+
+
+# In[ ]:
+
+
+
+
